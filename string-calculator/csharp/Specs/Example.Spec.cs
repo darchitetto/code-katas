71e5b1db89c622b60e656663608f96e84037cc29
darchitetto <denise@digital-pioneers.com>
2015-05-19 16:46:48 -0400

    getter done

--- string-calculator/csharp/Specs/Example.Spec.cs
+++ string-calculator/csharp/Specs/Example.Spec.cs
@@ -1,26 +1,136 @@
 ﻿using Given.Common;
 using Given.NUnit;
+using StringCalculator;
 
 namespace Specs
 {
-	public class when_adding_two_numbers : Scenario
+	public class when_adding_zero_numbers : Scenario
 	{
-		
-		given two_numbers = () =>
-		{
-			_input1 = 1;
-			_input2 = 2;
+		private static stringCalculator s;
 
+		given zero_numbers = () =>
+		{
+			s = new stringCalculator();
+			_input1 = "";
 		};
 
-		private when adding_two_numbers = () => _actual = _input1 + _input2;
+		private when adding_zero_numbers = () => _actual = s.Add(_input1);
 
-		private static int _input1;
-		private static int _input2;
+		private static string _input1;
 		private static int _actual;
 
 		[then]
-		public void it_should_return_the_summation_of_the_two_numbers()
+		public void it_should_return_the_summation_of_the_number()
+		{
+			_actual.ShouldEqual(0);
+		}
+	}
+
+	public class when_adding_one_number : Scenario
+	{
+		private static stringCalculator s;
+
+		given one_numbers = () =>
+		{
+			s = new stringCalculator();
+			_input1 = "1";
+		};
+
+		private when adding_one_numbers = () => _actual = s.Add(_input1);
+
+		private static string _input1;
+		private static int _actual;
+
+		[then]
+		public void it_should_return_the_summation_of_the_number()
+		{
+			_actual.ShouldEqual(1);
+		}
+	}
+
+	public class when_adding_two_numbers : Scenario
+	{
+		private static stringCalculator s;
+
+		given two_numbers = () =>
+		{
+			s = new stringCalculator();
+			_input1 = "1,2";
+		};
+
+		private when adding_two_numbers = () => _actual = s.Add(_input1);
+
+		private static string _input1;
+		private static int _actual;
+
+		[then]
+		public void it_should_return_the_summation_of_the_numbers()
+		{
+			_actual.ShouldEqual(3);
+		}
+	}
+
+	public class when_adding_many_numbers : Scenario
+	{
+		private static stringCalculator s;
+
+		given two_numbers = () =>
+		{
+			s = new stringCalculator();
+			_input1 = "1,2,3,4,5";
+		};
+
+		private when adding_many_numbers = () => _actual = s.Add(_input1);
+
+		private static string _input1;
+		private static int _actual;
+
+		[then]
+		public void it_should_return_the_summation_of_the_numbers()
+		{
+			_actual.ShouldEqual(15);
+		}
+	}
+
+	public class when_adding_two_numbers_seperated_by_newline : Scenario
+	{
+		private static stringCalculator s;
+
+		given two_numbers = () =>
+		{
+			s = new stringCalculator();
+			_input1 = "1\n2";
+		};
+
+		private when adding_two_numbers = () => _actual = s.Add(_input1);
+
+		private static string _input1;
+		private static int _actual;
+
+		[then]
+		public void it_should_return_the_summation_of_the_numbers()
+		{
+			_actual.ShouldEqual(3);
+		}
+	}
+
+	public class when_adding_two_numbers_seperated_by_delimeter : Scenario
+	{
+		private static stringCalculator s;
+
+		given two_numbers = () =>
+		{
+			s = new stringCalculator();
+			_input1 = "//[;]\n1,2";
+		};
+
+		private when adding_two_numbers = () => _actual = s.Add(_input1);
+
+		private static string _input1;
+		private static int _actual;
+
+		[then]
+		public void it_should_return_the_summation_of_the_numbers()
 		{
 			_actual.ShouldEqual(3);
 		}
---
+++ string-calculator/csharp/StringCalculator/stringCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+	public class stringCalculator
+	{
+		public int Add(string number)
+		{
+			var sum = 0;
+
+			if (!String.IsNullOrEmpty(number))
+			{
+				const string pattern = @"\/\/\[.*\]\\n";
+				var re = new Regex(pattern);
+
+				var delimeter = re.Matches(number);
+				number = re.Replace(number, string.Empty);
+
+
+				number = number.Replace('\n', ',');
+				number.Split(',').ToList().ForEach(x =>
+					sum += Convert.ToInt32(x));
+			}
+
+			return sum;
+		}
+	}
+}
